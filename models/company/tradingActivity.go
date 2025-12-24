52ec4800491a2f7aeede9e0d5a023d223c38cd8b
rawan-temo <rinas1122k@gmail.com>
2025-12-24 17:24:54 +0300

    fix: remove companies from the trading activity model

--- models/company/tradingActivity.go
+++ models/company/tradingActivity.go
@@ -5,6 +5,5 @@ import "github.com/Rawan-Temo/Baseet_Company_Registering.git/models"
 type TradingActivity struct {
 	models.NewGormModel
 	Name string `gorm:"uniqueIndex:idx_user_name_active,where:deleted_at IS NULL;type:varchar(100);not null" json:"name"`
-	// add many 2 many relation with company
-	Companies []Company `gorm:"many2many:company_trading_activities;" json:"companies"`
+
 }
